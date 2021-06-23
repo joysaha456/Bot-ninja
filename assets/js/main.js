504188cd5363138d2296baf78418d9e311f78f17
Joy Saha <joysaha544@gmail.com>
2021-06-23 12:13:21 +0600

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,66 @@
+$(document).ready(function () {
+   
+   // Fixed header demo
+   // $(window).on("scroll", function () {
+   //    if ($(window).scrollTop() > 150) {
+   //       $(".header").addClass("fixedHeader");
+   //    } else {
+   //       $(".header").removeClass("fixedHeader");
+   //    }
+   // });
+
+   // banner slider with swipper
+   // var mainbannerSlider = new Swiper(".mainbannerSlider", {
+   //    effect: "slide",
+   //    loop: true,
+
+   //    pagination: {
+   //       el: ".swiper-pagination",
+   //       clickable: true,
+   //    },
+
+   //    navigation: {
+   //       nextEl: ".swiper-button-next ",
+   //       prevEl: ".swiper-button-prev ",
+   //    },
+
+   //    scrollbar: {
+   //       el: ".swiper-scrollbar",
+   //    },
+   // });
+
+
+   // team section owl-carousel demo
+   // $("").owlCarousel({
+   //    loop: true,
+   //    items: 3,
+   //    dots: false,
+   //    nav: true,
+   //    navText: [
+   //       '<i class="fas fa-angle-left"></i>',
+   //       '<i class="fas fa-angle-right"></i>',
+   //    ],
+   //    margin: 30,
+   //    responsiveClass: true,
+   //    responsive: {
+   //       0: {
+   //          items: 1,
+   //          nav: true,
+   //       },
+   //       768: {
+   //          items: 2,
+   //          nav: true,
+   //       },
+   //       992: {
+   //          items: 3,
+   //          nav: true,
+   //       },
+   //    },
+   // });
+
+   // contact form select option
+   // $("select").niceSelect();
+
+   //WOW js 
+   // new WOW().init();
+});
